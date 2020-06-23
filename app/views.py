c417a457ced75586380d131ec3eac97556602df7
evgkarn <evgkarn@gmail.com>
2020-06-23 12:14:13 +0300

    image delete

--- app/views.py
+++ app/views.py
@@ -231,7 +231,7 @@ def delete_ad(ad_id):
     if ad is None:
         abort(404)
     if ad.image:
-        os.remove(ad.image)
+        os.remove('app' + ad.image)
     db.session.delete(ad)
     db.session.commit()
     return jsonify({'result': True})
