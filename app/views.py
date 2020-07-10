f223c30670bbeaacb8ddb41d7811f6b23ea91624
evgkarn <evgkarn@gmail.com>
2020-07-10 21:11:50 +0300

    orders shop

--- app/views.py
+++ app/views.py
@@ -277,7 +277,7 @@ def get_order_ads(shop_id):
             'shop_id': order.shop_id,
             'url': url_for('get_order', order_id=order.id, _external=True),
             'date_create': order.timestamp,
-            # 'shop': order.shop.name,
+            'shop': order.shop.name,
             # 'ad_id': order.post.id,
             # 'ad_name': order.post.name_ads,
         })
