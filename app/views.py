c327bbaff2c609bff475c50b7ac3f48a6bd01720
evgkarn <evgkarn@gmail.com>
2020-07-31 15:07:58 +0300

    pay

--- app/views.py
+++ app/views.py
@@ -977,4 +977,4 @@ def status_pay():
     file_path = config_local.APP_FOLDER + '/status.txt'
     with open(file_path, 'a') as fw:
         json.dump(request.json, fw)
-    return 'ОК', 200
+    return make_response("OK", 200)
