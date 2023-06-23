dec5a972a954019a6095ca4a58452610242d6a74
sablin <sablin.irk@gmail.com>
2023-06-23 17:50:56 +0800

    Пасхалка

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	let carYear =  document.querySelector('#carYear');
 	let cityIp =  document.querySelector('.cityIp');
 	let searchForm = document.querySelector('.search-form');
+	//пасхалка
 	let carImage = document.querySelector('.pashalka');
 
 	let url = String(window.location.href)
